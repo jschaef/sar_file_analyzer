b9e5b26f5ba70c0a24da030152d8d57978e7d589
jschaef <jochen.schaefer@suse.com>
2021-02-19 09:37:16 +0100

    removed unused color dictionary

--- code/config.py
+++ code/config.py
@@ -2,14 +2,6 @@
 
 class Config(object):
     upload_dir = 'upload'
-    colors = {'green': '#29910E',
-              'blue': '#004c97',
-              'red': '#FF0000',
-              'ligth_green': '#98FB98',
-              'light_blue': '#b2c1d3',
-              'ligth_red': '#FFA07A',
-              'gold': '#FFD700',
-              'brown': '#8B4513'}
     redis_host = 'localhost'
     redis_port = 6379
     rkey_pref = 'user'
