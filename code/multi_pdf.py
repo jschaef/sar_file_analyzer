689dedb21a8db66c86a6ae97b1b0b7f807f0ea4e
jschaef <jochen.schaefer@suse.com>
2023-02-06 11:34:49 +0100

    new for multi pdf writing

---
+++ code/multi_pdf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import os
+from PyPDF4 import PdfFileMerger
+
+def create_multi_pdf(pdf_field: list, outfile: str):
+    rm_field = pdf_field.copy()
+    merger = PdfFileMerger()
+    while len(pdf_field) > 0:
+        input = open(pdf_field.pop(0), "rb")
+        merger.append(input)
+    output = open(outfile, 'wb')
+    merger.write(output)
+    output.close()
+    for file in rm_field:
+        
+        print(file)
+        os.remove(file)
+    return outfile
